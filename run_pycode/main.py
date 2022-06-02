bbe6739a3c29696efb317afbc771062a3345148e
celmo <christianelmo@gmail.com>
2022-06-02 18:38:46 +0200

    Impletment actual action

--- run_pycode/main.py
+++ run_pycode/main.py
@@ -1,5 +1,16 @@
 import os
+import sys
 
 
 def main():
-    pass
+    sysarg = sys.argv[1]
+    key_arg = "--files-to-run="
+    if sysarg.startswith(key_arg):
+        paths = sysarg[len(key_arg):].replace('[','').replace(']','').replace(' ','').split(',')
+        for path in paths :
+            if path.endswith(".py"):
+                os.system(f"python {path}")
+            else:
+                print(f"{path} is not a python file")
+    else:
+        print("not valid argument")
